5ed89c20ee11b976209e1fd08988987c32dabe8b
thufir <hawat.thufir@gmail.com>
2019-06-09 22:35:44 -0700

    incremental

--- src/main/java/net/bounceme/dur/wildfly/war/maps/Result.java
+++ src/main/java/net/bounceme/dur/wildfly/war/maps/Result.java
@@ -8,8 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(value = "/Simple")
-public class Simple extends HttpServlet {
+@WebServlet(value = "/")
+public class Result extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
 
